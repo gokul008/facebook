de7e4038e4909ef4ff3f3fb1d0ae7f379c284b76
gokul008 <58761854+gokul008@users.noreply.github.com>
2019-12-13 09:08:37 +0530

    Update demo.java

--- src/test/java/facebook/demo.java
+++ src/test/java/facebook/demo.java
@@ -2,6 +2,8 @@ package facebook;
 
 public class demo {
 public static void main(String[] args) {
+	System.out.println("hello");
+	System.out.println("hiii");
 	System.out.println("hello world");
 	
 }
